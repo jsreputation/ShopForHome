7a5a52e857bffb75bf0d75cc8fbf53ac22eb90ce
Ash Palania <jsreputation@gmail.com>
2024-07-31 15:13:09 +0800

    init: first commit

---
+++ server/src/test/java/com/joshua/shopforhome/ShopforhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshua.shopforhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopforhomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
